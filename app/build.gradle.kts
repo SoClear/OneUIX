8ceb60dc8a6b522792c278c6865f23b1b6bdd86c
xrzcc <92078997+xrzcc@users.noreply.github.com>
2026-08-24 15:59:06 +0800

    chore: prepare personal.2 test build

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,8 +17,8 @@ android {
         applicationId = "io.github.soclear.oneuix"
         minSdk = 33
         targetSdk = 37
-        versionCode = 10
-        versionName = "1.7.0-personal.1"
+        versionCode = 11
+        versionName = "1.7.0-personal.2-test"
 
         ndk {
             // ABI过滤，只加载 arm 架构 64 位动态链接库
